b832934eb79054f9996024432c4c82a74c409d8b
Iwasaki Yudai <yudai@arielworks.com>
2015-08-28 19:43:47 +0900

    v0.0.5

--- gotty.rb
+++ gotty.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class Gotty < Formula
   homepage "https://github.com/yudai/gotty"
-  version 'v0.0.4'
+  version 'v0.0.5'
 
-  url "https://github.com/yudai/gotty/releases/download/v0.0.4/darwin_amd64.tar.gz"
-  sha1 "7ade35afb3d2d226820dea7c82c6bbf002220d0f"
+  url "https://github.com/yudai/gotty/releases/download/v0.0.5/darwin_amd64.tar.gz"
+  sha1 "fb4e723e95600d69a0fd2d99f39e00faa4e9c096"
 
   if OS.linux? && Hardware.is_64_bit?
-      url "https://github.com/yudai/gotty/releases/download/v0.0.4/linux_amd64.tar.gz"
-      sha1 "24eba326a7a099cda43b62dfd86eb64208088f38"
+      url "https://github.com/yudai/gotty/releases/download/v0.0.5/linux_amd64.tar.gz"
+      sha1 "bdb7bfcbfa27ce084ebd5e9d82127af4bd0fe28b"
   end
 
   depends_on :arch => :intel
