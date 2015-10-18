05f3a32ecd8aec33f1760ffd11f0f476bf9f1109
Iwasaki Yudai <yudai@arielworks.com>
2015-10-19 07:14:21 +0900

    Fix filename mismatch

--- gen.sh
+++ gen.sh
@@ -4,8 +4,8 @@ VERSION=`curl -s -L https://api.github.com/repos/yudai/gotty/releases/latest | j
 
 SHASUMS=`curl -s -L "https://github.com/yudai/gotty/releases/download/${VERSION}/SHASUMS"`
 
-SHA1_DARWIN=`echo "${SHASUMS}" | grep darwin_amd64.tar.gz | cut -f 1 -d ' '`
-SHA1_LINUX=`echo "${SHASUMS}" | grep linux_amd64.tar.gz | cut -f 1 -d ' '`
+SHA1_DARWIN=`echo "${SHASUMS}" | grep gotty_darwin_amd64.tar.gz | cut -f 1 -d ' '`
+SHA1_LINUX=`echo "${SHASUMS}" | grep gotty_linux_amd64.tar.gz | cut -f 1 -d ' '`
 
 echo "VERSION: ${VERSION}"
 echo "SHA1 DARWIN: ${SHA1_DARWIN}"
@@ -18,11 +18,11 @@ class Gotty < Formula
   homepage "https://github.com/yudai/gotty"
   version '$VERSION'
 
-  url "https://github.com/yudai/gotty/releases/download/$VERSION/darwin_amd64.tar.gz"
+  url "https://github.com/yudai/gotty/releases/download/$VERSION/gotty_darwin_amd64.tar.gz"
   sha1 "$SHA1_DARWIN"
 
   if OS.linux? && Hardware.is_64_bit?
-      url "https://github.com/yudai/gotty/releases/download/$VERSION/linux_amd64.tar.gz"
+      url "https://github.com/yudai/gotty/releases/download/$VERSION/gotty_linux_amd64.tar.gz"
       sha1 "$SHA1_LINUX"
   end
 
--- gotty.rb
+++ gotty.rb
@@ -4,11 +4,11 @@ class Gotty < Formula
   homepage "https://github.com/yudai/gotty"
   version 'v0.0.12'
 
-  url "https://github.com/yudai/gotty/releases/download/v0.0.12/darwin_amd64.tar.gz"
+  url "https://github.com/yudai/gotty/releases/download/v0.0.12/gotty_darwin_amd64.tar.gz"
   sha1 "60d6200e6c750481ebdc963de58987e6c4921ad5"
 
   if OS.linux? && Hardware.is_64_bit?
-      url "https://github.com/yudai/gotty/releases/download/v0.0.12/linux_amd64.tar.gz"
+      url "https://github.com/yudai/gotty/releases/download/v0.0.12/gotty_linux_amd64.tar.gz"
       sha1 "9d4330b62b220b606b739524c2b880916771bd2e"
   end
 
