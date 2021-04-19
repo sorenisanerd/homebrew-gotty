0cd0666ff1e1544d5b8d11b2f95054ea94359bf0
Søren L. Hansen <sorenisanerd@gmail.com>
2021-04-19 10:51:34 -0700

    Version != tag name

--- gen.sh
+++ gen.sh
@@ -1,12 +1,14 @@
 #/bin/bash -e
 
-VERSION=`curl -s -L https://api.github.com/repos/sorenisanerd/gotty/releases/latest | jq -r .tag_name`
+TAG_NAME=`curl -s -L https://api.github.com/repos/sorenisanerd/gotty/releases/latest | jq -r .tag_name`
+VERSION=${TAG_NAME#v}
 
-SHASUMS=`curl -s -L "https://github.com/sorenisanerd/gotty/releases/download/${VERSION}/SHA256SUMS"`
+SHASUMS=`curl -s -L "https://github.com/sorenisanerd/gotty/releases/download/${TAG_NAME}/SHA256SUMS"`
 
 SHA256_DARWIN=`echo "${SHASUMS}" | grep gotty_.*darwin_amd64.tar.gz | cut -f 1 -d ' '`
 SHA256_LINUX=`echo "${SHASUMS}" | grep gotty_.*linux_amd64.tar.gz | cut -f 1 -d ' '`
 
+echo "TAG_NAME: ${TAG_NAME}"
 echo "VERSION: ${VERSION}"
 echo "SHA256 DARWIN: ${SHA256_DARWIN}"
 echo "SHA256  LINUX: ${SHA256_LINUX}"
@@ -18,11 +20,11 @@ class Gotty < Formula
   homepage "https://github.com/sorenisanerd/gotty"
   version '$VERSION'
 
-  url "https://github.com/sorenisanerd/gotty/releases/download/${VERSION}/gotty_${VERSION}_darwin_amd64.tar.gz"
+  url "https://github.com/sorenisanerd/gotty/releases/download/${TAG_NAME}/gotty_${VERSION}_darwin_amd64.tar.gz"
   sha256 "$SHA256_DARWIN"
 
   if OS.linux? && Hardware.is_64_bit?
-      url "https://github.com/sorenisanerd/gotty/releases/download/${VERSION}/gotty_${VERSION}_linux_amd64.tar.gz"
+      url "https://github.com/sorenisanerd/gotty/releases/download/${TAG_NAME}/gotty_${VERSION}_linux_amd64.tar.gz"
       sha256 "$SHA256_LINUX"
   end
 
